382a346e36bea71df5ad1370594f8a77b3a72459
Lim Lon Hwee <limmumu@Lims-MacBook-Air.local>
2026-09-16 03:50:38 +0800

    Publish 辅成史王争霸战 site

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='fucheng-history-directory-v3';
+const ASSETS=['./','./index.html','./menu-style.css','./menu-banner-v3.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
