57b01355b74a5c8148649462909317b66ed584ac
Lim Lon Hwee <limmumu@Lims-MacBook-Air.local>
2026-09-16 08:42:02 +0800

    Refresh quiz cache for 150 questions

--- 四年级历史问答题/service-worker.js
+++ 四年级历史问答题/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='fucheng-history-year4-v3';
+const CACHE_NAME='fucheng-history-year4-v4';
 const ASSETS=['./','./index.html','./style.css','./branding.css','../compact-branding.css','../app.js','./company-logo.png','./questions.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
